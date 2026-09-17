b0a88a8e505f3ac2601fdce5b270b7edc2ac2de2
fillipehenrique <fillipehenrique007@gmail.com>
2026-09-17 04:27:30 -0300

    Create Variaveis.c

---
+++ Variaveis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 26;
+    int quantidade = 1;
+    float altura = 1.79;
+    char letra 'F';
+    char nome[20] = "Fillipe";
+
+    idade = 26;
+
+    quantidade = 10;
+    
+}
